b9c4b9a4640c92ab4e7a443d49136d47391d020c
Anna Anastasiadou <annaanast2002@gmail.com>
2025-10-27 16:53:17 +0200

    Build todoList for GitHub Pages deployment

--- JavaScript/todoList/webpack.config.js
+++ JavaScript/todoList/webpack.config.js
@@ -2,32 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/theOdinProject/JavaScript/todoList/dist/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TaskFlow Todo App',
+            template: './index.html',
+        }),
     ],
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
