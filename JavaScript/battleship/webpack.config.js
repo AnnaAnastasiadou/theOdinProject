3444c39b0a47b93b151f367f7c4e82977a4bd0a6
Anna Anastasiadou <annaanast2002@gmail.com>
2025-11-23 17:17:45 +0200

    Ensure relative asaset paths for production
    - package.json: Updated the 'build' script to use standard Unix syntax (`NODE_ENV=production`) to correctly set the environment variable without needing the 'cross-env' package.
    - webpack.config.js: Configured the `output.publicPath` to be an empty string (`''`) in production mode, which forces the `HtmlWebpackPlugin` to generate correct relative script tags (`src="main.js"`).

--- JavaScript/battleship/webpack.config.js
+++ JavaScript/battleship/webpack.config.js
@@ -1,19 +1,23 @@
 // webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../../docs/battleship'),
         clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+        // new MiniCssExtractPlugin({
+        //     filename: './src/style.css',
+        // }),
     ],
     module: {
         rules: [
@@ -21,6 +25,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            // {
+            //     test: /\.css$/i,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            // },
         ],
     },
 };
